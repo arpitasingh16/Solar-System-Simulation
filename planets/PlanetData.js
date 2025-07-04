90bfbd2bb9e8de9a11d687df02140603021915d5
arpitasingh16 <arpitasingh.rajput16@gmail.com>
2025-07-05 00:18:01 +0530

    real time relative speed

--- planets/PlanetData.js
+++ planets/PlanetData.js
@@ -1,10 +1,10 @@
 export const planetData = [
-  { name: 'Mercury', size: 2, distance: 20, texture: 'mercury.jpg', orbitSpeed: 4.17 },
-  { name: 'Venus',   size: 3, distance: 30, texture: 'venus.jpg', orbitSpeed: 1.61 },
-  { name: 'Earth',   size: 3.5, distance: 40, texture: 'earth.jpg', orbitSpeed: 1.0 },
-  { name: 'Mars',    size: 2.5, distance: 50, texture: 'mars.jpg', orbitSpeed: 0.53 },
-  { name: 'Jupiter', size: 6, distance: 65, texture: 'jupiter.jpg', orbitSpeed: 0.084 },
-  { name: 'Saturn',  size: 5.5, distance: 80, texture: 'saturn.jpg', orbitSpeed: 0.034 },
-  { name: 'Uranus',  size: 4.5, distance: 95, texture: 'uranus.jpg', orbitSpeed: 0.012 },
-  { name: 'Neptune', size: 4.2, distance: 110, texture: 'neptune.jpg', orbitSpeed: 0.006 }
+  { name: 'Mercury', size: 2, distance: 15, texture: 'mercury.jpg', orbitSpeed: 4.17 },
+  { name: 'Venus',   size: 3, distance: 22, texture: 'venus.jpg', orbitSpeed: 1.61 },
+  { name: 'Earth',   size: 3.5, distance: 30, texture: 'earth.jpg', orbitSpeed: 1.0 },
+  { name: 'Mars',    size: 2.5, distance: 38, texture: 'mars.jpg', orbitSpeed: 0.53 },
+  { name: 'Jupiter', size: 6, distance: 60, texture: 'jupiter.jpg', orbitSpeed: 0.084 },
+  { name: 'Saturn',  size: 5.5, distance: 85, texture: 'saturn.jpg', orbitSpeed: 0.034 },
+  { name: 'Uranus',  size: 4.5, distance: 115, texture: 'uranus.jpg', orbitSpeed: 0.012 },
+  { name: 'Neptune', size: 4.2, distance: 140, texture: 'neptune.jpg', orbitSpeed: 0.006 }
 ];
